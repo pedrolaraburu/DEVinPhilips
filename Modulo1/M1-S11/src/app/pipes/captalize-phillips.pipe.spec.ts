20897a24ddd97d9918a147de8359770494dece80
Pedro Laraburu <pedrolaraburu@gmail.com>
2022-12-17 18:49:43 -0300

    Revert "Add ex11"
    This reverts commit efb59daca661284acef864665cfd5a4da5759d42.

--- Modulo1/M1-S11/src/app/pipes/captalize-phillips.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CaptalizePhillipsPipe } from './captalize-phillips.pipe';
-
-describe('CaptalizePhillipsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CaptalizePhillipsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
